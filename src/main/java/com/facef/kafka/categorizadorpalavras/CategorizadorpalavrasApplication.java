ea6cbffce3e7c4296c41cd2bf4939109cf27d66e
Caio Poli <caio.poli@ernetwork.com.br>
2020-09-18 22:12:31 -0300

    :tada: Begin a project

---
+++ src/main/java/com/facef/kafka/categorizadorpalavras/CategorizadorpalavrasApplication.java
@@ -0,0 +1,13 @@
+package com.facef.kafka.categorizadorpalavras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategorizadorpalavrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategorizadorpalavrasApplication.class, args);
+	}
+
+}
